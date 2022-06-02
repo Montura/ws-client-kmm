779ceab13aea34b7506e032737338a053019f81f
Andrei Mikhalev <amikhalev@devexperts.com>
2022-06-02 16:01:00 +0300

    Comment other ios platforms

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -10,9 +10,9 @@ version = "1.0"
 
 kotlin {
 //    android()
-    iosX64()
+//    iosX64()
     iosArm64()
-    iosSimulatorArm64()
+//    iosSimulatorArm64()
 
     cocoapods {
         summary = "Some description for the Shared Module"
@@ -43,26 +43,26 @@ kotlin {
 //            }
 //        }
 //        val androidTest by getting
-        val iosX64Main by getting
+//        val iosX64Main by getting
         val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
+//        val iosSimulatorArm64Main by getting
         val iosMain by creating {
             dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
+//            iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
+//            iosSimulatorArm64Main.dependsOn(this)
             dependencies {
                 implementation("io.ktor:ktor-client-darwin:$ktorVersion")
             }
         }
-        val iosX64Test by getting
+//        val iosX64Test by getting
         val iosArm64Test by getting
-        val iosSimulatorArm64Test by getting
+//        val iosSimulatorArm64Test by getting
         val iosTest by creating {
             dependsOn(commonTest)
-            iosX64Test.dependsOn(this)
+//            iosX64Test.dependsOn(this)
             iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
+//            iosSimulatorArm64Test.dependsOn(this)
         }
     }
 }
