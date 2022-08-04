4cae981309fd4c4b76844d53e9c36a7c2362ac06
Andrei Mikhalev <amikhalev@devexperts.com>
2022-08-04 14:09:18 +0300

    clean up

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -3,17 +3,13 @@ val ktorVersion: String by project
 plugins {
     kotlin("multiplatform")
     kotlin("native.cocoapods")
-//    id("com.android.library")
 }
 
 version = "1.0"
 
 kotlin {
-//    android()
-//    iosX64()
     jvm()
     iosArm64()
-//    iosSimulatorArm64()
 
     cocoapods {
         summary = "Some description for the Shared Module"
@@ -30,53 +26,19 @@ kotlin {
             dependencies {
                 runtimeOnly("org.jetbrains.kotlin:kotlin-reflect:1.7.0")
                 implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
-                implementation(kotlin("stdlib-jdk8"))
                 implementation("io.ktor:ktor-client-core:$ktorVersion")
                 implementation("io.ktor:ktor-client-cio:$ktorVersion")
                 implementation("io.ktor:ktor-client-websockets:$ktorVersion")
             }
         }
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-            }
-        }
         val jvmMain by getting
-//        val androidMain by getting {
-//            dependencies {
-//                implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
-//            }
-//        }
-//        val androidTest by getting
-//        val iosX64Main by getting
         val iosArm64Main by getting
-//        val iosSimulatorArm64Main by getting
         val iosMain by creating {
             dependsOn(commonMain)
-//            iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
-//            iosSimulatorArm64Main.dependsOn(this)
             dependencies {
                 implementation("io.ktor:ktor-client-darwin:$ktorVersion")
             }
         }
-//        val iosX64Test by getting
-        val iosArm64Test by getting
-//        val iosSimulatorArm64Test by getting
-        val iosTest by creating {
-            dependsOn(commonTest)
-//            iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
-//            iosSimulatorArm64Test.dependsOn(this)
-        }
     }
 }
-
-//android {
-//    compileSdk = 31
-//    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-//    defaultConfig {
-//        minSdk = 23
-//        targetSdk = 31
-//    }
-//}
