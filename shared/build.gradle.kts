d5ba1f3571a74d336b974ed3a5f747ea0f693bd8
Andrei Mikhalev <amikhalev@devexperts.com>
2022-08-04 15:02:49 +0300

    clean up android

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,17 +4,13 @@ plugins {
     kotlin("multiplatform")
     kotlin("native.cocoapods")
     kotlin("plugin.serialization") version "1.6.21"
-//    id("com.android.library")
 }
 
 version = "1.0"
 
 kotlin {
-//    android()
-//    iosX64()
     jvm()
     iosArm64()
-//    iosSimulatorArm64()
 
     cocoapods {
         summary = "Some description for the Shared Module"
@@ -38,35 +34,15 @@ kotlin {
         val jvmMain by getting {
             dependencies {
                 implementation("io.ktor:ktor-client-cio:$ktorVersion")
-                implementation(kotlin("stdlib-jdk8"))
             }
         }
-//        val androidMain by getting {
-//            dependencies {
-//                implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
-//            }
-//        }
-//        val androidTest by getting
-//        val iosX64Main by getting
         val iosArm64Main by getting
-//        val iosSimulatorArm64Main by getting
         val iosMain by creating {
             dependsOn(commonMain)
-////            iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
-////            iosSimulatorArm64Main.dependsOn(this)
             dependencies {
                 implementation("io.ktor:ktor-client-darwin:$ktorVersion")
             }
         }
     }
 }
-
-//android {
-//    compileSdk = 31
-//    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-//    defaultConfig {
-//        minSdk = 23
-//        targetSdk = 31
-//    }
-//}
